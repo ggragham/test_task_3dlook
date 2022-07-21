c550af64a71cba4685a3562e0cd95ccc0db3bfd4
Grell Gragham <vrt.epm@protonmail.com>
2022-07-21 22:36:17 +0300

    Add: deployment Django and pgAdmin via ansible

---
+++ files/config_local.py
@@ -0,0 +1,5 @@
+LOG_FILE = '/var/log/pgadmin/pgadmin.log'
+SQLITE_PATH = '/var/lib/pgadmin/pgadmin.db'
+SESSION_DB_PATH = '/var/lib/pgadmin/sessions'
+STORAGE_DIR = '/var/lib/pgadmin/storage'
+SERVER_MODE = True
